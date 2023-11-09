d81246b4d9abdf8589125599115959b9e7013ac5
hchuzhong <hchuzhong@163.com>
2023-11-09 11:09:45 +0800

    add items test unit

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::ItemsController < ApplicationController
     def index
-        items = Item.page(params[:page]).per(50)
+        items = Item.page(params[:page]).per(10)
         render json: {resources: items, pager: {
             page: params[:page],
-            per_page: 100,
+            per_page: 10,
             count: Item.count
         }}
     end
     def create 
-        item = Item.new amount: 1
+        item = Item.new amount: params[:amount]
         if item.save
-            render json: {resouce: item}
+            render json: {resource: item}
         else
             render json: {errors: item.errors}
         end
