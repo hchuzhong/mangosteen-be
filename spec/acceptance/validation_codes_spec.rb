9381ddfa4e182bb1b58254240d1599a76c6e879e
hchuzhong <hchuzhong@163.com>
2023-12-17 23:33:01 +0800

    mail could be sent and received

--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -5,8 +5,8 @@ resource "Validation Codes" do
     post "/api/v1/validation_codes" do
         parameter :email, type: :string
         let(:email) { 'test@qq.com' }
-
         example "send validation code request" do
+            expect(UserMailer).to receive(:welcome_email).with(email)
             do_request
             expect(status).to eq 200
             expect(response_body).to eq ' '
