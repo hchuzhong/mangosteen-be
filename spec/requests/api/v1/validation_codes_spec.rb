07b525dbfa556f452f76763e6718b66cf758be70
hchuzhong <hchuzhong@163.com>
2023-12-19 17:09:14 +0800

    jwt would expired after two hours

--- spec/requests/api/v1/validation_codes_spec.rb
+++ spec/requests/api/v1/validation_codes_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "ValidationCodes", type: :request do
   describe "send validation_codes" do
     it "return 429 with too many requests" do
-      post '/api/v1/validation_codes', params: {email: 'hchuzhong@163.com'}
+      post '/api/v1/validation_codes', params: {email: 'test@qq.com'}
       expect(response).to have_http_status(200)
-      post '/api/v1/validation_codes', params: {email: 'hchuzhong@163.com'}
+      post '/api/v1/validation_codes', params: {email: 'test@qq.com'}
       expect(response).to have_http_status(429)
     end
   end
