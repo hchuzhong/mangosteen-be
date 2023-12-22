f676f28c009651cf4e242cd017afa39cd5799e1b
hchuzhong <hchuzhong@163.com>
2023-12-22 12:58:29 +0800

    reject invalid email when request Validation Codes

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,12 +1,11 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
     return render status: :too_many_requests if ValidationCode.exists?(email: params[:email], kind: 'sign_in', created_at: 1.minute.ago..Time.now)
-    
     validation_code = ValidationCode.new email: params[:email], kind: 'sign_in'
     if validation_code.save
-      render status: 200
+      render status: :ok
     else
-      render json: {errors: validation_code.errors}, status: 400
+      render json: {errors: validation_code.errors}, status: :unprocessable_entity
     end
   end
 end
