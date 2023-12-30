272551c56df07d92e1f05ee5ac0965503476e04b
hchuzhong <hchuzhong@163.com>
2023-12-30 11:51:47 +0800

    update pack_for_remote.sh

--- spec/acceptance/mes_spec.rb
+++ spec/acceptance/mes_spec.rb
@@ -10,7 +10,6 @@ resource "Me" do
       do_request
       expect(status).to eq 200
       json = JSON.parse response_body
-      p json
       expect(json['resource']['id']).to eq current_user.id
     end
   end
