dbaa039f776f9c1bbe44280aa53feb1c03ae2b1e
hchuzhong <hchuzhong@163.com>
2023-12-30 16:31:12 +0800

    typo

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
     def welcome_email(email)
         validation_code = ValidationCode.order(created_at: :desc).find_by_email(email)
         @code = validation_code.code
-        mail(to: email, subject: `[#{@code}]Mangosteen Validation Code`)
+        mail(to: email, subject: "[#{@code}]Mangosteen Validation Code")
     end
 end
