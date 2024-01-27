017027208b9e5114c4c00aeac7255afb274e816d
hchuzhong <hchuzhong@163.com>
2024-01-27 11:05:02 +0000

    update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "8.134.183.52:8080"
+        origins "8.134.183.52:8080", "http://mangosteen.hchuzhong.top"
         resource "*",
             methods: [:get, :post, :put, :patch, :delete, :options, :head],
             headers: :any,
